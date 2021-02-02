e5cd79394da1d0b01fa1b3b10effd82d0ae07cdf
HackBL <bojms45@gmail.com>
2021-02-02 02:27:49 -0500

    Setup global variable URL

--- pages/register/register.vue
+++ pages/register/register.vue
@@ -2,7 +2,7 @@
 	<view>
 		<image src="../../static/logo-2.png" class="logo" mode="widthFix"></image>
 		<view class="register-container">
-			<input type="text" placeholder="输入你的邀请码" class="register-code" maxlength="6" v-model="registerCode" />
+			<input type="text" placeholder="输入你的激活码" class="register-code" maxlength="6" v-model="registerCode" />
 			<view class="register-desc">管理员创建员工证账号之后，你可以从你的个人邮箱中获得注册邀请码</view>
 			<button class="register-btn" open-type="getUserInfo" @tap="register()">执行注册</button>
 		</view>
@@ -21,7 +21,7 @@
 				uni.login({
 					provider:"weixin",
 					success:function(resp){
-						// console.log(resp.code)
+						console.log(resp.code)
 						let code = resp.code
 						uni.getUserInfo({
 							provider:"weixin",
